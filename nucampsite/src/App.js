d51a7c10a8ce2fef22c183b3c77a1199ca1359ea
Jayprox <jayadeguzman@gmail.com>
2023-08-13 14:50:24 -0700

    Adding Partners. Week 2

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
 import './App.css';
-import CampsitesList from './features/campsites/CampsiteList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='about' element={<AboutPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ nucampsite/src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import SubHeader from "../components/SubHeader";
+import { Container } from 'reactstrap';
+
+const ContactPage = () => {
+    return (
+        <Container>
+            <SubHeader current='Contact'/>
+        </Container>
+    );
+}
+
+export default ContactPage;
